74640a65ff052708e972241c5647022ca397f429
ItsJoey29 <daems.joeyy@gmail.com>
2021-11-28 13:36:32 +0100

    SOD Sunday

--- client/book-list/src/App.js
+++ client/book-list/src/App.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    {
+      fetch("/books").then((response) =>
+        response.json().then((data) => {
+          setBooks(data.books);
+        })
+      );
+    }
+  }, []);
+
+  return null;
 };
 
 export default App;
