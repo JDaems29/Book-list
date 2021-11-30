474c166ec06fbca5ec6169a328af98baeff79cdd
ItsJoey29 <daems.joeyy@gmail.com>
2021-11-30 12:36:22 +0100

    Added semantic ui

--- client/book-list/src/App.js
+++ client/book-list/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Books from "./components/Books";
 
 const App = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    {
-      fetch("/books").then((response) =>
-        response.json().then((data) => {
-          setBooks(data.books);
-        })
-      );
-    }
+    fetch("/books").then((response) =>
+      response.json().then((data) => {
+        setBooks(data.Books);
+      })
+    );
   }, []);
 
-  return null;
+  console.log(books);
+
+  return (
+    <div className="App">
+      <Books books={books} />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/book-list/src/components/Books.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Books = ({ books }) => {
+  return <div>{books.length}</div>;
+};
+
+export default Books;
